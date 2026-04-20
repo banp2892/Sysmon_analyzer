8199125ae8cd564c5eb1db72fada92889ea8028a
banp2892 <86781747+banp2892@users.noreply.github.com>
2026-04-20 18:52:12 +0000

    Deploying to gh-pages from @ banp2892/Sysmon_analyzer@83002a8298b44bda343c48abbc0ca9916d00b13b 🚀

--- classPreparationData.js
+++ classPreparationData.js
@@ -1,6 +1,6 @@
 var classPreparationData =
 [
     [ "CalculateEntropy", "classPreparationData.html#ae5392d95a42f5d4a9aca7e2b1966f3d7", null ],
-    [ "PrepareNetwork", "classPreparationData.html#a74fd630244fbd012c7aac6fcc854d5ca", null ],
-    [ "PrepareProcess", "classPreparationData.html#aee35cfe8ba76f780f199c0cc1635c3f5", null ]
+    [ "PrepareNetwork", "classPreparationData.html#a96e2eb6d048fae8d8ebf0f99b5510616", null ],
+    [ "PrepareProcess", "classPreparationData.html#a27f6ac68cf42bc8dcc4c4ab0f7a6a137", null ]
 ];
--- search/functions_5.js
+++ search/functions_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['parseandlog_0',['ParseAndLog',['../classSysmonCollector.html#a213457f8ac7e0a0a3691a89386605959',1,'SysmonCollector']]],
-  ['preparenetwork_1',['PrepareNetwork',['../classPreparationData.html#a74fd630244fbd012c7aac6fcc854d5ca',1,'PreparationData']]],
-  ['prepareprocess_2',['PrepareProcess',['../classPreparationData.html#aee35cfe8ba76f780f199c0cc1635c3f5',1,'PreparationData']]]
+  ['preparenetwork_1',['PrepareNetwork',['../classPreparationData.html#a96e2eb6d048fae8d8ebf0f99b5510616',1,'PreparationData']]],
+  ['prepareprocess_2',['PrepareProcess',['../classPreparationData.html#a27f6ac68cf42bc8dcc4c4ab0f7a6a137',1,'PreparationData']]]
 ];
